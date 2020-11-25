a59744642bd7bc630c9bdb6f41cd8b35adf9f84e
Opilane TTHK <opilane@tthk.ee>
2020-11-25 13:52:40 +0200

    first commit

---
+++ DeleteFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeleteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Fridaywork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fridaywork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
